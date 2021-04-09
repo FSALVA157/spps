ab868eb71033beb991b67864069c88a47e373bc7
pedro <pedrodiaz0487@gmail.com>
2021-04-09 20:11:18 -0300

    crud de tablas sexo y estado-civil

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USERNAME } from './config/constants';
+import { SexoModule } from './sexo/sexo.module';
+import { EstadoCivilModule } from './estado-civil/estado-civil.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>(DATABASE_HOST),
+        //host: "127.0.0.1",
+        port: parseInt(config.get<string>(DATABASE_PORT),10),
+        //port: 3306,
+        //username: config.get<string>(DATABASE_USERNAME),
+        username:"root",
+        //password: config.get<string>(DATABASE_PASSWORD),
+        password:"spps",
+        //database: config.get<string>(DATABASE_NAME),
+        database: "spps_db",
+        entities: [__dirname + "./**/**/*.entity{.ts,.js}"],
+        autoLoadEntities: true,
+        synchronize: true,
+      }) 
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    SexoModule,
+    EstadoCivilModule
+
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
