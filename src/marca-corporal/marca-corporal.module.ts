0bf120ddabe379a818d2c1d8931ee264c0ea4b7c
pedro <pedrodiaz0487@gmail.com>
2021-10-21 04:14:49 -0300

    marca corporal terminado

--- src/marca-corporal/marca-corporal.module.ts
+++ src/marca-corporal/marca-corporal.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MarcaCorporalController } from './marca-corporal.controller';
 import { MarcaCorporalService } from './marca-corporal.service';
+import { MarcaCorporal } from './entities/marca-corporal.entity';
 
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+        MarcaCorporal
+    ])
+  ],
   controllers: [MarcaCorporalController],
   providers: [MarcaCorporalService]
 })
--- src/marca-corporal/marca-corporal.service.ts
+++ src/marca-corporal/marca-corporal.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { EditMarcaCorporalDto } from './dto/edit-marca-corporal.dto';
 import { MarcaCorporal } from './entities/marca-corporal.entity';
+import { CreateMarcaCorporalDto } from './dto/create-marca-corporal.dto';
 
 @Injectable()
 export class MarcaCorporalService {
@@ -55,6 +56,30 @@ export class MarcaCorporalService {
         } catch (error) {
             throw new Error(error.message);            
         }
+    }
+
+    /**
+     * Servicio que elimina un registro de la tabla JURISDICCION según id
+     * se utiliza remove y no delete porque solo el primero activa triggers
+     * @param id 
+     * @returns 
+     */
+     async deleteOne(id: number){
+        const respuesta = await this.marcaRepository.findOne(id);
+        if(!respuesta) throw new NotFoundException("No existe el registro de Juzgado que desea eliminar");
+        return await this.marcaRepository.remove(respuesta);        
+    }
+
+    /**
+     * Servicio que crea un nuevo registro de la tabla JURISDICCION 
+     * @param data 
+     * @returns 
+     */
+    async createOne(data: CreateMarcaCorporalDto){
+        const existe = await this.marcaRepository.findOne({detalle: data.detalle});
+        if(existe) throw new BadRequestException("El registro marca corporal ya existe");
+        const nuevo = this.marcaRepository.create(data);
+        return await this.marcaRepository.save(nuevo)
     }
 
 
